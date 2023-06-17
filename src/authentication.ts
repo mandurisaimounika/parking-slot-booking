fc31886a1e8bd4d81cf8aebfd43abfc604a640f6
mandurisaimounika <mandurisaimounika@gmail.com>
2023-06-17 17:51:40 +0200

    added authentication and permission checking files

---
+++ src/authentication.ts
@@ -0,0 +1,34 @@
+import {
+  ExtendedRequest
+} from "./userInterface";
+import * as jwt from 'jsonwebtoken';
+import {
+  executeQuery
+} from "./database/dataQuery";
+
+// Middleware to authenticate users with an API token
+const authenticateToken = async (req: ExtendedRequest, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({
+      error: 'Unauthorized'
+    });
+  }
+
+  const decoded = jwt.decode(token);
+  const text = 'SELECT * FROM users WHERE firstname = $1 AND lastname = $2';
+  const values = [decoded?.firstName, decoded?.lastName];
+  const user = await executeQuery(text, values);
+
+  if (!user[0]) {
+    return res.status(401).json({
+      error: 'Invalid token'
+    });
+  }
+
+  req.user = user[0];
+  next();
+};
+
+export default authenticateToken;
---
+++ src/checkPermissions.ts
@@ -0,0 +1,16 @@
+import {
+  ExtendedRequest
+} from "./userInterface";
+
+// Middleware to check user role
+const permissionCheck = (req: ExtendedRequest, res, next) => {
+  if (req.user.role === 'admin' || req.user.role === 'standard') {
+    next();
+  } else {
+    res.status(403).json({
+      error: 'Unauthorized access'
+    });
+  }
+};
+
+export default permissionCheck;
