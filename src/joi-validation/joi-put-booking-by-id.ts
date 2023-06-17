2aa2545b815071a016041606d24d263edc5ed953
mandurisaimounika <mandurisaimounika@gmail.com>
2023-06-17 19:25:38 +0200

    Prettier formated files

--- src/joi-validation/joi-put-booking-by-id.ts
+++ src/joi-validation/joi-put-booking-by-id.ts
@@ -1,21 +1,17 @@
-import {
-    validate,
-    Joi
-} from 'express-validation';
+import { validate, Joi } from "express-validation";
 
 //Middleware validation check for put request
 export function bookingsByIdAndSpotValidation() {
-    return {
-        params: Joi.object({
-            id: [Joi.string().uuid(), Joi.string().min(5).max(
-                50).trim().required()]
-        }),
-        body: Joi.object({
-            parkingSpot: Joi.string().min(1).required()
-        })
-    };
+  return {
+    params: Joi.object({
+      id: [Joi.string().uuid(), Joi.string().min(5).max(50).trim().required()],
+    }),
+    body: Joi.object({
+      parkingSpot: Joi.string().min(1).required(),
+    }),
+  };
 }
 
 export function bookingsByIdAndSpoitValidationMiddleware() {
-    return validate(bookingsByIdAndSpotValidation());
+  return validate(bookingsByIdAndSpotValidation());
 }
